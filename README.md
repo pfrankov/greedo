80c25c4f181b514364b46c6fe0c0023f2dc0a958
Pavel Frankov <pavel@frankov.ru>
2017-03-26 23:06:54 +0300

    initial commit of Greedo

---
+++ README.md
@@ -0,0 +1,13 @@
+# :alien: Greedo
+[![npm version](https://badge.fury.io/js/greedo.svg)](https://www.npmjs.com/package/greedo)  
+Bootstrap-like flexbox-based responsive grid system.
+
+http://pfrankov.github.io/greedo/
+
+## Installation
+
+```bash
+npm install --save greedo
+# or
+git clone https://github.com/pfrankov/greedo
+```
---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+var banner =
+		"   ------------------------------------------------\n"+
+		"      👽 Greedo \n"+
+		"      Flexbox-based responsive grid system\n"+
+		"   ------------------------------------------------\n"+
+		"   Author: Pavel Frankov   twitter: @twenty\n"+
+		"   Fork me on Github: https://github.com/pfrankov/greedo";
+
+console.log(banner);
+
+module.exports = {
+	plugins: {
+		autoprefixer: {
+			browsers: ["last 2 versions"]
+		},
+		"css-mqpacker": {},
+		"cssnano": {},
+		"postcss-banner": {
+			banner: banner
+		}
+	}
+};
